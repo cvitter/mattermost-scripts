29e98cce2c897c6fb82b3baddf10fcbb0c4799a2
Craig Vitter <craig.vitter@gmail.com>
2019-01-10 16:17:52 -0500

    Update get_users_in_channels.sql
    Fix tabs

--- get_users_in_channels.sql
+++ get_users_in_channels.sql
@@ -4,24 +4,24 @@
 # Note: Returns only Public ('O') and Private ('P') channels
 # Tested on MySQL 5.7.24
 SELECT
-	Teams.DisplayName AS Team,
-	Channels.DisplayName AS Channel,
+  Teams.DisplayName AS Team,
+  Channels.DisplayName AS Channel,
   Channels.Type,
   Users.LastName,
   Users.FirstName,
   Users.Username
 FROM
-	mattermost.ChannelMembers
+  mattermost.ChannelMembers
 JOIN
-	mattermost.Channels ON ChannelMembers.ChannelId = Channels.Id
+  mattermost.Channels ON ChannelMembers.ChannelId = Channels.Id
 JOIN
-	mattermost.Teams ON Channels.TeamId = Teams.Id
+  mattermost.Teams ON Channels.TeamId = Teams.Id
 JOIN
   mattermost.Users ON ChannelMembers.UserId = Users.Id
 WHERE
-	Channels.DeleteAt = 0 AND
+  Channels.DeleteAt = 0 AND
   Channels.Type <> 'D' AND
   Teams.DeleteAt = 0 AND
   Users.DeleteAt = 0
 ORDER BY
-	Team, Channel, LastName, Username;
+  Team, Channel, LastName, Username;
