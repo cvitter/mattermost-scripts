c53d512cebb122b40877efbbf09ffbb480e330e5
Craig Vitter <craig.vitter@gmail.com>
2019-01-30 09:05:53 -0500

    Create get_posts_by_team_and_channel.sql

---
+++ get_posts_by_team_and_channel.sql
@@ -0,0 +1,21 @@
+# The following SQL query retrieves a list of active channels
+# by team and the total number of posts in each channel
+# ordered by Team and Channel.
+# Note: This query returns only Public ('O') and Private ('P') 
+# channels and posts that have not been deleted.
+# Tested on MySQL 5.7.24
+SELECT
+    Teams.DisplayName AS Team,
+    Channels.DisplayName AS Channel,
+    Channels.Type,
+    (SELECT COUNT(*) FROM mattermost.Posts WHERE Posts.ChannelId = Channels.Id AND Posts.DeleteAt = 0) AS Posts
+FROM
+    mattermost.Channels
+JOIN
+    mattermost.Teams ON Channels.TeamId = Teams.Id
+WHERE
+    Channels.DeleteAt = 0 AND
+    Channels.Type <> 'D' AND
+    Teams.DeleteAt = 0
+ORDER BY
+    Team, Channel;
