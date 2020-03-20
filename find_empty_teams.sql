475fb7384bae6396778de4508851888831bc03a6
Andreas Böttger <andreas.boettger@tuhh.de>
2020-03-20 18:14:56 +0100

    Create find_empty_teams.sql

---
+++ find_empty_teams.sql
@@ -0,0 +1,18 @@
+SELECT
+    Teams.DisplayName AS Team,
+    COUNT(Teams.DisplayName) AS Members,
+    SUM( CASE WHEN TeamMembers.DeleteAt = 0 THEN 1 ELSE 0 END ) AS Active,
+    SUM( CASE WHEN TeamMembers.DeleteAt > 0 THEN 1 ELSE 0 END ) AS Deleted
+FROM 
+    Teams
+LEFT JOIN
+    TeamMembers ON TeamMembers.TeamId = Teams.Id
+LEFT JOIN
+    Users ON TeamMembers.UserId = Users.Id
+WHERE
+    Teams.DeleteAt = 0
+GROUP BY
+    Team
+ORDER BY
+    Team
+;
